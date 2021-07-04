98a8719da39af7a5303049060a62bd74579cbd07
Rodrigo Ramos <rodrigo.ramos.santos@gmail.com>
2021-07-04 19:21:48 -0300

    experiment linux pawershell

--- build/windows-build.ps1
+++ build/windows-build.ps1
@@ -6,13 +6,13 @@ param (
 	[Parameter(Mandatory)]
 	$PackageName
 )
-$version = ${env:GITHUB_REF} -replace 'refs/\w+/', ''
+$version = ${env:GITHUB_REF} -replace 'refs/\w+/w+', ''
 Write-Output "Restoring dependencies"
 dotnet restore "$SolutionPath"
 
 Write-Output "Publishing app"
-dotnet publish "$SolutionPath" --output "$PublishFolder" --configuration Release --version-suffix "$version"
+dotnet publish "$SolutionPath" --output "$PublishFolder" --configuration Release /p:version="$version"
 
-Write-Output "Zipping files"
-cd "$PublishFolder" && tar.exe -a -c -f "$PackageName" *.* Template/*.* -C "$PublishFolder" && cd ..
+#Write-Output "Zipping files"
+#cd "$PublishFolder" && tar.exe -a -c -f "$PackageName" *.* Template/*.* -C "$PublishFolder" && cd ..
 

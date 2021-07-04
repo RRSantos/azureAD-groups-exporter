97fff11ce05cfe27cc08c03ad1690779d6468e3a
Rodrigo Ramos <rodrigo.ramos.santos@gmail.com>
2021-07-04 19:02:58 -0300

    experiments with powershell build

--- build/windows-build.ps1
+++ build/windows-build.ps1
@@ -1,2 +1,18 @@
+param (
+	[Parameter(Mandatory)]
+	$SolutionPath,
+	[Parameter(Mandatory)]
+	$PublishFolder,
+	[Parameter(Mandatory)]
+	$PackageName
+)
 $version = ${env:GITHUB_REF} -replace 'refs/\w+/', ''
-Write-Host "$version"
+Write-Output "Restoring dependencies"
+dotnet restore "$SolutionPath"
+
+Write-Output "Publishing app"
+dotnet publish "$SolutionPath" --output "$PublishFolder" --configuration Release --version-suffix "$version"
+
+Write-Output "Zipping files"
+cd "$PublishFolder" && tar.exe -a -c -f "$PackageName" *.* Template/*.* -C "$PublishFolder" && cd ..
+
